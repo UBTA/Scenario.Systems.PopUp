901cfc5ba2aacf0f1b56800532b496785cdeed94
Weackss <62299994+UBTA@users.noreply.github.com>
2024-11-07 19:51:10 +0300

    1.2.5

---
+++ Runtime/PopUpSystem.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using EblanDev.ScenarioCore.GameFramework.Systems;
+using EblanDev.ScenarioCore.UIFramework.Bus;
+using UnityEngine;
+
+namespace EblanDev.ScenarioCore.Systems.PopUpSystemUnit
+{
+    public class PopUpSystem : SystemBase
+    {
+        [SerializeField] private List<PopUpView> popUps;
+        
+        private static List<PopUpView> popUpsSt;
+
+        public override void Prepare() {}
+
+        public override void Init()
+        {
+            popUpsSt = popUps;
+
+            foreach (var popUp in popUps)
+            {
+                popUp.Init();
+            }
+        }
+
+        public static void SetBus(UIBus bus)
+        {
+            foreach (var popUp in popUpsSt)
+            {
+                if (popUp is IBusPopUp<UIBus> busPopUp)
+                {
+                    busPopUp.SetBus(bus);
+                }
+            }
+        }
+
+        public static T Get<T>() where T : PopUpView
+        {
+            foreach (var popUp in popUpsSt)
+            {
+                if (popUp.GetType() == typeof(T))
+                {
+                    return (T) popUp;
+                }
+            }
+            return null;
+        }
+        
+        public override void Exit(bool saveData) {}
+    }
+}
